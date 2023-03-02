f77034afab266601b68d657c23f268bd96537e89
Exiels <Samina29@outlook.fr>
2023-03-02 02:17:50 +0100

    [ADD] Added admin competencies full page without style. [MOD] Temporary removed anti DDOS in backend

--- frontend/src/admin/settings/competencies/competencies.js
+++ frontend/src/admin/settings/competencies/competencies.js
@@ -1,9 +1,13 @@
-import {useLayoutEffect} from "react"
+import {useEffect, useLayoutEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import Header from "../../../components/header/admin/header"
+import Table from 'react-bootstrap/Table';
 import './competencies.scss'
 
 export default function Home() {
+    const [competencesList, setCompetencesList] = useState([]);
+    const [competenceName, setCompetenceName] = useState('');
+    const [competenceLevel, setCompetenceLevel] = useState('');
     let navigate = useNavigate()
 
     useLayoutEffect(() => {
@@ -30,13 +34,106 @@ export default function Home() {
         checkLogin()
     }, [])
 
+    async function getCompetencies() {
+        const response = await fetch("http://localhost:8080/administration/competencies", {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const data = await response.json();
+        console.log("data : " + data.competencies)
+        setCompetencesList(data.competencies);
+    }
+
+    async function addCompetencies() {
+        const token = sessionStorage.getItem("token");
+        await fetch("http://localhost:8080/administration/competencies", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-auth-token': token
+            },
+            body: JSON.stringify({
+                name: competenceName,
+                level: competenceLevel
+            })
+        })
+    }
+
+    async function delCompetence(data) {
+        console.log(data.ID)
+        const token = sessionStorage.getItem("token");
+        await fetch("http://localhost:8080/administration/competencies", {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-auth-token': token
+            },
+            body: JSON.stringify({
+                id: data._id
+            })
+        })
+        window.location.reload(false);
+    }
+
+    useEffect(() => {
+        getCompetencies()
+    }, [])
+
+    const changeCompetenceName = (event) => {
+        setCompetenceName(event.target.value)
+    }
+
+    const changeCompetenceLevel = (event) => {
+        setCompetenceLevel(event.target.value)
+    }
+
     return (
         <div>
             <div>
                 <Header />
             </div>
-            <div>
+            <div id={"mainDiv"}>
                 <p>Admin Competencies page</p>
+                <div id={"competenciesDiv"}>
+                    <div id="competenciesAdderDiv">
+                        <form>
+                            <div>
+                                <input id="competenceNameInput" type={"text"} placeholder={"competenceName"} onChange={changeCompetenceName} required/>
+                            </div>
+                            <div>
+                                <input id="competenceLevelInput" type={"text"} placeholder={"competenceLevel"} onChange={changeCompetenceLevel} required/>
+                            </div>
+                            <div>
+                                <button onClick={addCompetencies}>
+                                    Submit
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                    <div id="competenciesList">
+                        <Table id="accountTable">
+                            <thead id="tableHead">
+                            <tr id="topTable">
+                                <th id="valHead1">Name</th>
+                                <th id="valHead2">Level</th>
+                            </tr>
+                            </thead>
+                            <tbody id="tableBody">
+                                {
+                                    competencesList.map((data, index) =>
+                                        <tr key={index}>
+                                            <td>{data.name}</td>
+                                            <td>{data.level}</td>
+                                            <td><button onClick={() => delCompetence(data)}>X</button></td>
+                                        </tr>
+                                    )
+                                }
+                            </tbody>
+                        </Table>
+                    </div>
+                </div>
             </div>
         </div>
     )
