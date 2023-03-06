449bbe2928b5eb3190503dd1384b61b0176cd068
Exiels <Samina29@outlook.fr>
2023-03-06 21:13:48 +0100

    [ADD] Added user projects

--- frontend/src/user/projects/projects.js
+++ frontend/src/user/projects/projects.js
@@ -1,16 +1,45 @@
-import {useState} from "react"
-import { useNavigate } from "react-router-dom"
 import Header from "../../components/header/user/header"
 import './projects.scss'
 
 export default function Home() {
+    const [projectsList, setProjectsList] = useState([]);
+
+    useLayoutEffect(() => {
+        async function getProjects() {
+            try {
+                const response = await fetch("http://localhost:8080/admin/github", {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                const data = await response.json();
+                setProjectsList(data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getProjects()
+    }, [])
+
     return (
         <div>
             <div>
                 <Header />
             </div>
-            <div>
+            <div id={"mainDiv"}>
                 <p>Projects page</p>
+                <div id="projectListDiv">
+                    {
+                        projectsList.map((data, index) =>
+                            <div id={"data_" + index}>
+                                <a href={data.html_url} title={data.name} id="title"></a>
+                                <p id="description">data.description</p>
+                                <p id="language" >data.language</p>
+                            </div>
+                        )
+                    }
+                </div>
             </div>
         </div>
     )
